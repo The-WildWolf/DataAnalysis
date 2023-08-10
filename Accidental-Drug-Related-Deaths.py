4159125c48a2fff2444b24ccd72750ffd1fcfe8f
The-WildWolf <ksenna.sitek@gmail.com>
2023-08-10 14:45:40 +0200

    after recreating venv it's working

--- Accidental-Drug-Related-Deaths.py
+++ Accidental-Drug-Related-Deaths.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
 # link to dataset and loading it using pandas library
 dataset_url = 'https://code.datasciencedojo.com/datasciencedojo/datasets/raw/master/Accidental%20Drug%20Related' \
               '%20Deaths%20in%20Connecticut,%20US/Accidental%20Drug%20Related%20Deaths%20in%20Connecticut-2012-2018.csv'
