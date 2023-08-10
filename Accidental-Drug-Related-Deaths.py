467b79d3985f05d5ee7c812133ca8a9f4ce4c960
The-WildWolf <ksenna.sitek@gmail.com>
2023-08-10 14:39:46 +0200

    fist data analysis should working, but Warn to fix: UserWarning: Matplotlib is currently using agg, which is a non-GUI backend, so cannot show the figure.   plt.show()
    Process finished with exit code 0

--- Accidental-Drug-Related-Deaths.py
+++ Accidental-Drug-Related-Deaths.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # link to dataset and loading it using pandas library
 dataset_url = 'https://code.datasciencedojo.com/datasciencedojo/datasets/raw/master/Accidental%20Drug%20Related' \
@@ -6,5 +8,19 @@ dataset_url = 'https://code.datasciencedojo.com/datasciencedojo/datasets/raw/mas
 
 dataset = pd.read_csv(dataset_url)
 
+# which drugs are most associated with deaths
 
+drugs = dataset.columns[21:38]
+# print(drugs)
+each_drug = dataset[drugs].apply(lambda col: col.value_counts().get('Y', 0))
+
+# bar chart for "which drugs are most associated with deaths"
+plt.figure(figsize=(12,6))
+sns.barplot(x=each_drug.index, y=each_drug.values)
+plt.title("which drugs are most associated with deaths")
+plt.xlabel("Type of the drug")
+plt.ylabel("How many times found in dead body")
+plt.xticks(rotation=90)
+plt.tight_layout()
+plt.show()
 
